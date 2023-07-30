a4ecf633d4c530fdf9c6499e6c1d61bda14b6f45
elossio <evlossio@gmail.com>
2023-07-30 17:13:39 -0300

    Initial commit

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ from kivy.uix.screenmanager import Screen
 class HomePage(Screen):
     pass
 
+class AjustesPAge(Screen):
+    pass
+
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
     def build(self):
         return GUI
+
+    def mudar_tela(self, id_tela):
+        self.root
+        gerenciador_telas = self.root.ids['screen_manager']
+        gerenciador_telas.current = id_tela
 
 
 MainApp().run()
